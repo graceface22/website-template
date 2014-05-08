4c20eedda5c7109b893b6c853015fd4d8acece17
sikessle <sikessle@MBA-silver.local>
2014-05-08 18:12:15 +0200

    implemented grunt watch and less task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,34 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    }
-  });
+    // Project configuration.
+    grunt.initConfig({
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+        pkg: grunt.file.readJSON('package.json'),
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+        watch: {
+            style: {
+                files: ['stylesheets/**/*.less'],
+                tasks: ['less']
+            }
+        },
+
+        less: {
+            development: {
+                files: {
+                    "stylesheets/main.min.css": "stylesheets/main.less"
+                },
+                options: {
+                    compress: true
+                }
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task(s).
+    grunt.registerTask('default', ['watch']);
 
 };
--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 Website template for HTML5 based websites with Twitter bootstrap and other javascript stuff based on bower.io
 
 ## Getting Started
+- Run 'npm install'
 - Run 'bower install'
-- Run 'grunt watch' (not working atm)
+- Run 'grunt watch' to recompile on change .less files
