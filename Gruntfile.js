cd6bdc1125e0745c3c51f34fa2488d31e49dc467
sikessle <sikessle@MBA-silver.local>
2014-11-19 10:13:04 +0100

    added css autoprefixer as grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,18 +8,27 @@ module.exports = function (grunt) {
         watch: {
             style: {
                 files: ['stylesheets/**/*.less'],
-                tasks: ['less']
+                tasks: ['less_compile', 'autoprefixer']
             }
         },
 
-        less: {
+        less_compile: {
             development: {
                 files: {
                     "stylesheets/main.min.css": "stylesheets/main.less"
                 },
                 options: {
-                    compress: true
+                    compress: false
                 }
+            }
+        },
+
+        autoprefixer: {
+            options: {
+                browsers: ['last 2 versions', 'ie 8', 'ie 9']
+            },
+            no_dest: {
+                src: "stylesheets/main.min.css" // will be overwritten
             }
         }
 
@@ -27,6 +36,11 @@ module.exports = function (grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+
+    // Overwriting less task to execute autoprefixer
+    grunt.task.renameTask('less', 'less_compile');
+    grunt.registerTask('less', ['less_compile', 'autoprefixer']);
 
     // Default task(s).
     grunt.registerTask('default', ['watch']);
